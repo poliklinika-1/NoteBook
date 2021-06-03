ae167ffe90e2db43769c5bbc05abd3f6890461a8
Seric0 <Pol1kz2020@gmail.com>
2021-06-03 16:33:28 +0600

    Fix code

---
+++ NoteBook/Models/NoteBooks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NoteBook.Models
+{
+    public class NoteBooks
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Введите наименование блокнота!")]
+        [Display(Name = "Наименование блокнота:")]
+        public string Title { get; set; }
+        public DateTime Notebook_date { get; set; }
+    }
+
+    public class NoteWithBook
+    {
+
+        public NoteWithBook()
+        {
+
+        }
+        public NoteWithBook(NoteBooks notebook)
+        {
+            //TO DO: Complete Member Initialization  
+            this.NoteBooks = notebook;
+        }
+        public NoteBooks NoteBooks { get; set; }
+        public Note Note { get; set; }
+    }
+}
